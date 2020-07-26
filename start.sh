d8a68937db661f160d6e3485a40bd22f7e4fb688
jnztech <accounts@jnztech.co.za>
2020-07-26 23:14:38 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 echo "Starting start.sh.."
+echo "Starting supercronic.."
 supercronic /etc/periodic/hourly/one
+echo "Starting dpos-api-fallback.."
 cd /home/rise/rise-pool/dpos-api-fallback && npm start -- -n https://wallet.rise.vision -s R
