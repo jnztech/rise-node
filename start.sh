be0888a8e19f3b21da4f1861022f58a2631f55ff
jnztech <accounts@jnztech.co.za>
2020-07-27 00:33:28 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,3 +2,5 @@
 echo "Starting start.sh.."
 echo "Starting dpos-api-fallback.."
 cd /home/rise/rise-pool/dpos-api-fallback && npm start -- -n https://wallet.rise.vision -s R
+echo "Start and tail cron.."
+cron && tail -f /var/log/cron.log
