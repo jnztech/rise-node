98d8deafe4b56f2c6eb820a5d91f329acf6dfa31
jnztech <accounts@jnztech.co.za>
2020-07-26 21:55:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
 echo "Starting start.sh.."
-sudo echo "*       *       *       *       *       run-parts /etc/periodic/1min" >> /etc/crontabs/root
-sudo crontab -l
-sudo /usr/bin/crond -f -l 8
